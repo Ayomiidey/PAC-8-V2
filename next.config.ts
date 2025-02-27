6a1946aab7051624924d29a494ea7c0121cc5055
Ayomiidey <quadriayomiidey@gmail.com>
2025-02-27 02:22:26 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["cdn.bestsuppliers.com"],
+  },
 };
 
 export default nextConfig;
