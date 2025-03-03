23a5c33d78e722f8de0e6a8e40f7033500d429d5
Ayomiidey <quadriayomiidey@gmail.com>
2025-03-03 18:32:37 +0100

    sign-in auth

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+const SignInPage = () => {
+  return <>SignIn</>;
+};
+
+export default SignInPage;
