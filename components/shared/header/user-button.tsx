742916cd8e0f9b385fd38a7cab741fe49c5f4e1b
Ayomiidey <quadriayomiidey@gmail.com>
2025-03-06 15:18:42 +0100

    user profile

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -1,8 +1,16 @@
 import { auth } from "@/auth";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-// import { signOutUser } from "@/lib/actions/user.action";
+import { signOutUser } from "@/lib/actions/user.action";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { UserIcon } from "lucide-react";
+import { DropdownMenuLabel } from "@radix-ui/react-dropdown-menu";
+
 const UserButton = async () => {
   const session = await auth();
   if (!session) {
@@ -23,7 +31,46 @@ const UserButton = async () => {
       </>
     );
   }
-  return <div>User</div>;
+
+  const firstInitial = session.user?.name?.charAt(0).toUpperCase() ?? "U";
+  return (
+    <div className="flex gap-2 items-center">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className="flex items-center">
+            <Button
+              variant="ghost"
+              className="w-8 h-8 ml-2 rounded-full bg-gray-200"
+            >
+              {firstInitial}
+            </Button>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <div className="text-sm font-medium leading-none">
+                {session.user?.name}
+              </div>
+              <div className="text-sm text-muted-foreground leading-none">
+                {session.user?.email}
+              </div>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuItem className="p-0 mb-1">
+            <form action={signOutUser} className="w-full">
+              <Button
+                variant="ghost"
+                className="w-full py-4 px-2 h-4 justify-start"
+              >
+                Sign Out
+              </Button>
+            </form>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 };
 
 export default UserButton;
