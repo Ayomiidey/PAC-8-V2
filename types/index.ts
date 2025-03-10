f92c4855dc3d53df14fc5f6df85ec239e4d09347
Ayomiidey <quadriayomiidey@gmail.com>
2025-03-10 20:39:54 +0100

    'm

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,14 @@
 import { z } from "zod";
-import { insertProductSchema } from "@/lib/validators";
+import {
+  cartItemSchema,
+  insertCartSchema,
+  insertProductSchema,
+} from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: string;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
