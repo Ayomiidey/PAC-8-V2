02120151e8b183d7cd718e65b14ed7199ef962a6
Ayomiidey <quadriayomiidey@gmail.com>
2025-03-17 22:51:33 +0100

    cart action

--- lib/actions/cart.action.tsx
+++ lib/actions/cart.action.tsx
@@ -2,25 +2,18 @@
 
 import { cookies } from "next/headers";
 import { CartItem } from "@/types";
-import { formatError } from "../utils";
+import { convertToPlainObject, formatError } from "../utils";
 import { auth } from "@/auth";
 import { prisma } from "@/db/prisma";
 
 export async function addItemToCart(data: CartItem) {
   try {
     const sessionCartId = (await cookies()).get("sessionCartId")?.value;
-    if (!sessionCartId) {
-      throw new Error("Cart session not found");
-    }
+    if (!sessionCartId) throw new Error("Cart session not found");
 
     //Get session and userId
     const session = await auth();
     const userId = session?.user?.id ? (session.user.id as string) : undefined;
-
-    //Get Cart from DB
-    const cart = prisma.cart.findFirst({
-      where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
-    });
 
     return {
       success: true,
@@ -32,4 +25,30 @@ export async function addItemToCart(data: CartItem) {
       message: formatError(error),
     };
   }
+}
+
+export async function getMyCart() {
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+  if (!sessionCartId) throw new Error("Cart session not found");
+
+  // Get session and user ID
+  const session = await auth();
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+  // Get user cart from database
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
+
+  if (!cart) return undefined;
+
+  // Convert Decimal values to strings
+  return convertToPlainObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
 }
