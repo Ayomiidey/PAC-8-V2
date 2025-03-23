659874326f78952275e63abfb575d3d27d9bec29
Ayomiidey <quadriayomiidey@gmail.com>
2025-03-23 17:27:37 +0100

    currency formatter

--- lib/utils.ts
+++ lib/utils.ts
@@ -51,3 +51,19 @@ export function round2(value: number | string) {
     throw new Error("Value not a String or Number");
   }
 }
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
+  currency: "USD",
+  style: "currency",
+  minimumFractionDigits: 2,
+});
+
+export function formatCurrency(amount: number | string | null) {
+  if (typeof amount === "number") {
+    return CURRENCY_FORMATTER.format(amount);
+  } else if (typeof amount === "string") {
+    return CURRENCY_FORMATTER.format(Number(amount));
+  } else {
+    return "NaN";
+  }
+}
