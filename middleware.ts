eadbb3c745d41b11fda3fce7b887ce9c22da6f7a
Ayomiidey <quadriayomiidey@gmail.com>
2025-04-12 10:38:52 +0100

    fix

--- middleware.ts
+++ middleware.ts
@@ -32,27 +32,6 @@ export async function middleware(request: any) {
     return NextResponse.redirect(signInUrl);
   }
 
-  // const session = await auth();
-
-  // const protectedPaths = [
-  //   "/shipping-address",
-  //   "/payment-method",
-  //   "/place-order",
-  //   "/profile",
-  //   /\/user\/(.*)/,
-  //   /\/order\/(.*)/, // regex: matches  /order/anything
-  //   /\/admin\/(.*)/,
-  // ];
-
-  // const isProtectedRoute = protectedPaths.some((path) =>
-  //   request.nextUrl.pathname.startsWith(path)
-  // );
-  // // If the route is protected and there's no session, redirect to sign-in
-  // if (isProtectedRoute && !session) {
-  //   const signInUrl = new URL("/sign-in", request.url);
-  //   signInUrl.searchParams.set("callbackUrl", request.url);
-  //   return NextResponse.redirect(signInUrl);
-  // }
   // Check if the sessionCartId cookie exists
   const sessionCartId = request.cookies.get("sessionCartId")?.value;
 
